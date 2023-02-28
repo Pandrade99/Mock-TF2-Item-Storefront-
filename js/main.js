095ecb3edbc28e585da07fa7e25cd36caf4a97d9
Pandrade99 <pandrade@umd.edu>
2023-02-27 20:01:45 -0500

    added quantity and calculated total price functionality

--- js/main.js
+++ js/main.js
@@ -12,3 +12,62 @@ cartIcon.onclick = () =>{
 closeCart.onclick = () =>{
     cart.classList.remove('active')
 };
+
+// Cart Working JS
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready();
+}
+
+// Making Function
+function ready(){
+// Remove Items From cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for (var i = 0; i < removeCartButtons.length; i++){
+        var button = removeCartButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+    // quantity Changes
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
+    }
+}
+
+// Remove Items From cart 
+function removeCartItem(event){
+    var buttonClicked = event.target
+    buttonClicked.parentElement.remove()
+    updatetotal();
+}
+
+//Quantity Changes
+function quantityChanged(event){
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1
+    }
+
+    updatetotal();
+}
+
+
+// Update Total
+function updatetotal(){
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var price = parseFloat(priceElement.innerText.replace('$', ""));
+        var quantity = quantityElement.value; 
+        total = total + price * quantity;
+
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+    }
+}
